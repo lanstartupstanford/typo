771e851db763806911225a5cb296f0b74a4af4fb
lanstartupstanford <lan.startup.stanford@gmail.com>
2013-11-17 08:52:21 +0700

    fix bug for edit html

--- app/models/article.rb
+++ app/models/article.rb
@@ -418,12 +418,13 @@ class Article < Content
 
   def merge_with(other_article_id)
     tomerge = Article.find_by_id(other_article_id)
-    if not self.id or not tomerge.id
+    if not self.id or not tomerge
       return false
     end
     
     self.body = self.body + "\n\n" + tomerge.body
     self.comments << tomerge.comments
+    debugger
     self.save!
     
     tomerge = Article.find_by_id(other_article_id)
